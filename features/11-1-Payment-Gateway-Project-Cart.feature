fe78e6b5975856bb1bb6ac786e29cb3bae511f0a
jona-terceros <jonatan.terceros@ucb.edu.bo>
2023-12-01 13:25:45 -0400

    Added 11-1 Payment Gateway Project Cart E2

--- features/11-1-Payment-Gateway-Project-Cart.feature
+++ features/11-1-Payment-Gateway-Project-Cart.feature
@@ -3,11 +3,22 @@ Feature: Buy a Mother Elephant With Babies Soft Toy
 As a toy purchasing customer,
     To buy a toy elephant,
     I want to verify if my payment was made successfully
+Background:
+    Given I am on the payment gateway project
+    When I choose the quantity of elephants
+    | Quantity: | 6      |
+    And I click the "Buy Now" button b
 
 Scenario: choose the quantity of elephant toys to buy
-  Given I am on the payment gateway project
-  When I choose the quantity of elephants
-  | Quantity: | 6      |
-  And I click the "Buy Now" button b
-  Then I should be able to see the payment process
+  Then I should be able to see the payment process and the price
  
+ Scenario: fill out the payment detail fields
+  When I enter the required fields payment
+  | Card Number:       | 4825088890201232  |
+  | Expiration Month:  | 11                |
+  | Expiration Year:   | 2025              |
+  | CVV Code:          | 677               |
+  And I press the "Pay" button pb
+  Then I should be able to see the message payment successfull and my order id
+ 
+
--- features/step_definitions/(11-1) Cart/Cart.rb
+++ features/step_definitions/(11-1) Cart/Cart.rb
@@ -18,9 +18,37 @@ And(/^I click the "Buy Now" button b$/) do
     find(:xpath, xpath_base).click
   end
 
-Then(/^I should be able to see the payment process$/) do
+Then(/^I should be able to see the payment process and the price$/) do
     payment_process_webpage = "Payment Process"
     expect(page).to have_content(payment_process_webpage)
+    expect(page).to have_css('#three > div > form > div.row > div', text: '$120.00')
+  end
+
+When(/^I enter the required fields payment$/) do |table|
+    data = table.rows_hash
+    data.each_pair do |key, value|
+      case key
+    when "Card Number:"
+        fill_in 'card_nmuber', :with => value
+    when "Expiration Month:"
+        select(value, :from => 'month')
+    when "Expiration Year:"
+        select(value, :from => 'year')
+    when "CVV Code:"
+        fill_in 'cvv_code', :with => value
+      end
+    end
+  end
+  And(/^I press the "Pay" button pb$/) do
+    xpath_base = '/html/body/section/div/form/div[2]/div/ul/li/input'
+    find(:xpath, xpath_base).click
+  end
+  Then(/^I should be able to see the message payment successfull and my order id$/) do
+    payment_successfull = "Payment successfull!"
+    expect(page).to have_content(payment_successfull)
+
+    second_cell_element = find(:xpath, '//table[@class="alt access"]/tbody/tr/td[2]')
+    value_in_second_cell = second_cell_element.text
+    expect(value_in_second_cell).not_to be_empty
 
   end
-  
